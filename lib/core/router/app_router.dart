93ae0472ca27b9b5780f08c6050990d70c2418c9
farisha <153209114+Farisha13@users.noreply.github.com>
2025-09-23 20:32:28 +0700

    Add routing for GamePage with query parameters for attempts count and word length

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:word_game/features/game/presentation/page/game_page.dart';
 import 'package:word_game/features/home/presentation/home_page.dart';
 
 class AppRouter {
@@ -9,6 +10,19 @@ class AppRouter {
         path: HomePage.route,
         builder: (context, state) {
           return HomePage();
+        },
+      ),
+      GoRoute(
+        path: '/game',
+        builder: (context, state) {
+          return GamePage(
+            attemptsCount: int.parse(
+              state.uri.queryParameters['attemptsCount'] ?? '',
+            ),
+            wordLength: int.parse(
+              state.uri.queryParameters['wordLength'] ?? '',
+            ),
+          );
         },
       ),
     ],
