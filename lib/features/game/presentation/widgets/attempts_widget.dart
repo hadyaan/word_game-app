641190af6759ed30adf694db8d1e2f1ce170940f
farisha <153209114+Farisha13@users.noreply.github.com>
2025-09-25 13:02:20 +0700

    Refactor AttemptsWidget to use BlocBuilder and implement AttemptRowWidget for dynamic attempts display; add LetterBoxWidget for visual representation of letters.

--- lib/features/game/presentation/widgets/attempts_widget.dart
+++ lib/features/game/presentation/widgets/attempts_widget.dart
@@ -1,16 +1,32 @@
 import 'package:flutter/widgets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:word_game/features/game/presentation/bloc/game_bloc.dart';
+import 'package:word_game/features/game/presentation/bloc/game_state.dart';
+import 'package:word_game/features/game/presentation/widgets/attempt_row_widget.dart';
 
 class AttemptsWidget extends StatelessWidget {
   const AttemptsWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemBuilder: (context, index) {},
-      separatorBuilder: (context, index) {
-        return SizedBox(height: 10);
+    return BlocBuilder<GameBloc, GameState>(
+      builder: (context, state) {
+        return Center(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * .9,
+            child: ListView.separated(
+              itemBuilder: (context, index) {
+                return AttemptRowWidget(attemptIndex: index);
+              },
+              separatorBuilder: (context, index) {
+                return SizedBox(height: 10);
+              },
+              itemCount: state.attemptsCount ?? 0,
+              shrinkWrap: true,
+            ),
+          ),
+        );
       },
-      itemCount: 10,
     );
   }
 }
