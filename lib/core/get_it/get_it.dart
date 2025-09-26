64c767ae516cdf8847c0b89dffe0b067f340cbc9
farisha <153209114+Farisha13@users.noreply.github.com>
2025-09-26 17:41:37 +0700

    Refactor dependency registration in get_it.dart to improve setup clarity

--- lib/core/get_it/get_it.dart
+++ lib/core/get_it/get_it.dart
@@ -1,15 +1,28 @@
 import 'package:get_it/get_it.dart';
+import 'package:word_game/core/api/dio_client.dart';
+import 'package:word_game/features/game/data/datasource/game_remote_datasource.dart';
+import 'package:word_game/features/game/data/repository/game_repository_impl.dart';
+import 'package:word_game/features/game/domain/game_repository.dart';
 import 'package:word_game/features/game/presentation/bloc/game_bloc.dart';
 
 var getIt = GetIt.instance;
 
 void setup() {
+  registerDatasources();
+  registerRepositories();
   registerBloc();
 }
 
-void registerRepositories(){
-  // TODO
+void registerDatasources() {
+  getIt.registerSingleton(GameRemoteDatasource(dio: DioClient.getDio()));
 }
+
+void registerRepositories() {
+  getIt.registerSingleton<GameRepository>(
+    GameRepositoryImpl(gameRemoteDatasource: getIt()),
+  );
+}
+
 void registerBloc() {
   getIt.registerFactory<GameBloc>(() => GameBloc(gameRepository: getIt()));
 }
